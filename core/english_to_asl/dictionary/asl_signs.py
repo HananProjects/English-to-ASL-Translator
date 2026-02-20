5b460eb46ee6e1cd605b7f1587a836fd0f204668
HananProjects <155928130+HananProjects@users.noreply.github.com>
2026-02-20 11:44:38 -0600

    removed the .json clips and videos as they were oriented incorrectly

--- core/english_to_asl/dictionary/asl_signs.py
+++ core/english_to_asl/dictionary/asl_signs.py
@@ -23,11 +23,11 @@ ASL_SIGNS = {
         "duration": 0.8,
     },
     "GO": {
-        "clips": ["go_1", "go_1_2", "go_2", "go_3"],
+        "clips": ["go_1", "go_2", "go_3"],
         "duration": 0.7,
     },
     "AND": {
-        "clips": ["and_1", "and_2"],
+        "clip": "and_2",
         "duration": 0.7,
     },
     "HOME": {
@@ -35,43 +35,43 @@ ASL_SIGNS = {
         "duration": 0.7,
     },
     "WHY": {
-        "clips": ["why_1", "why_2"],
+        "clip": "why_2",
         "duration": 0.7,
     },
     "WHERE": {
-        "clips": ["where_1", "where_1_2"],
+        "clip": "where_1",
         "duration": 0.7,
     },
     "BROTHER": {
-        "clips": ["brother_1", "brother_1_2", "brother_1_3", "brother_2", "brother_3"],
+        "clips": ["brother_1", "brother_2", "brother_3"],
         "duration": 0.7,
     },
     "DRINK": {
-        "clips": ["drink_1", "drink_1_2", "drink_1_3", "drink_2", "drink_3"],
+        "clips": ["drink_1", "drink_2", "drink_3"],
         "duration": 0.7,
     },
     "EAT": {
-        "clips": ["eat_1", "eat_1_2", "eat_1_3", "eat_2", "eat_3"],
+        "clips": ["eat_1", "eat_2", "eat_3"],
         "duration": 0.7,
     },
     "FAMILY": {
-        "clips": ["family_1", "family_1_2", "family_1_3", "family_2", "family_3"],
+        "clips": ["family_1", "family_2", "family_3"],
         "duration": 0.8,
     },
     "FINISH": {
-        "clips": ["finish_1", "finish_1_2", "finish_1_3", "finish_2", "finish_3"],
+        "clips": ["finish_1", "finish_2", "finish_3"],
         "duration": 0.7,
     },
     "FORGET": {
-        "clips": ["forget_1", "forget_1_2", "forget_2", "forget_3"],
+        "clips": ["forget_1", "forget_2", "forget_3"],
         "duration": 0.8,
     },
     "GIVE": {
-        "clips": ["give_1", "give_1_2", "give_2", "give_3"],
+        "clips": ["give_1", "give_2", "give_3"],
         "duration": 0.7,
     },
     "HELP": {
-        "clips": ["help_1", "help_1_2", "help_2", "help_3"],
+        "clips": ["help_1", "help_2", "help_3"],
         "duration": 0.7,
     },
     "HERE": {
@@ -79,51 +79,51 @@ ASL_SIGNS = {
         "duration": 0.7,
     },
     "LATER": {
-        "clips": ["later_1", "later_1_2", "later_2", "later_3"],
+        "clips": ["later_1", "later_2", "later_3"],
         "duration": 0.7,
     },
     "LIKE": {
-        "clips": ["like_1", "like_1_2", "like_2", "like_3"],
+        "clips": ["like_1", "like_2", "like_3"],
         "duration": 0.7,
     },
     "MAN": {
-        "clips": ["man_1", "man_1_2", "man_2", "man_3"],
+        "clips": ["man_1", "man_2", "man_3"],
         "duration": 0.7,
     },
     "MOTHER": {
-        "clips": ["mother_1", "mother_1_2", "mother_2", "mother_3"],
+        "clips": ["mother_1", "mother_2", "mother_3"],
         "duration": 0.7,
     },
     "MY": {
-        "clips": ["my_1", "my_2"],
+        "clip": "my_2",
         "duration": 0.7,
     },
     "NEED": {
-        "clips": ["need_1", "need_1_2", "need_2", "need_3"],
+        "clips": ["need_1", "need_2", "need_3"],
         "duration": 0.7,
     },
     "NO": {
-        "clips": ["no_1", "no_1_2", "no_2", "no_3"],
+        "clips": ["no_1", "no_2", "no_3"],
         "duration": 0.7,
     },
     "NOW": {
-        "clips": ["now_1", "now_1_2", "now_2", "now_3"],
+        "clips": ["now_1", "now_2", "now_3"],
         "duration": 0.7,
     },
     "PLAY": {
-        "clips": ["play_1", "play_1_2", "play_2", "play_3"],
+        "clips": ["play_1", "play_2", "play_3"],
         "duration": 0.7,
     },
     "PLEASE": {
-        "clips": ["please_1", "please_2", "please_3"],
+        "clip": "please_2",
         "duration": 0.7,
     },
     "SCHOOL": {
-        "clips": ["school_1", "school_1_2", "school_2", "school_3"],
+        "clips": ["school_1", "school_2", "school_3"],
         "duration": 0.7,
     },
     "TIME": {
-        "clips": ["time_1", "time_1_2", "time_2", "time_3"],
+        "clips": ["time_1", "time_2", "time_3"],
         "duration": 0.7,
     },
     "TOMORROW": {
@@ -136,47 +136,47 @@ ASL_SIGNS = {
         "duration": 0.7,
     },
     "WAIT": {
-        "clips": ["wait_1", "wait_1_2", "wait_2", "wait_3"],
+        "clips": ["wait_1", "wait_2", "wait_3"],
         "duration": 0.7,
     },
     "WANT": {
-        "clips": ["want_1", "want_1_2", "want_2", "want_3"],
+        "clips": ["want_1", "want_2", "want_3"],
         "duration": 0.7,
     },
     "WATER": {
-        "clips": ["water_1", "water_1_2", "water_2", "water_3"],
+        "clips": ["water_1", "water_2", "water_3"],
         "duration": 0.7,
     },
     "WE": {
-        "clips": ["we_1", "we_2"],
+        "clip": "we_2",
         "duration": 0.7,
     },
     "WHAT": {
-        "clips": ["what_1", "what_1_2", "what_2", "what_3"],
+        "clips": ["what_1", "what_2", "what_3"],
         "duration": 0.7,
     },
     "WHO": {
-        "clips": ["who_1", "who_1_2", "who_2", "who_3"],
+        "clips": ["who_1", "who_2", "who_3"],
         "duration": 0.7,
     },
     "WOMAN": {
-        "clips": ["woman_1", "woman_1_2", "woman_2", "woman_3"],
+        "clips": ["woman_1", "woman_2", "woman_3"],
         "duration": 0.7,
     },
     "WORK": {
-        "clips": ["work_1", "work_1_2", "work_2", "work_3"],
+        "clips": ["work_1", "work_2", "work_3"],
         "duration": 0.7,
     },
     "YEAR": {
-        "clips": ["year_1", "year_1_2", "year_2", "year_3"],
+        "clips": ["year_1", "year_2", "year_3"],
         "duration": 0.7,
     },
     "YES": {
-        "clips": ["yes_1", "yes_1_2", "yes_2", "yes_3"],
+        "clips": ["yes_1", "yes_2", "yes_3"],
         "duration": 0.7,
     },
     "YOUR": {
-        "clips": ["your_1", "your_2"],
+        "clip": "your_2",
         "duration": 0.7,
     },
 }
